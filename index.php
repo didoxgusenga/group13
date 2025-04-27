f87955e63cda337fa12211a7bcec2afbe3653bf0
uwimanapatrick14209 <uwimanapatro2018@gmail.com>
2025-04-27 12:36:51 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+// PHP code first or inside the HTML later
+$hash = hash('sha256', 'GROUP ASSIGNMENT 13 PHP');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>GROUP ASSIGNMENT 13 PHP</title>
+</head>
+<body>
+    <h1>GROUP ASSIGNMENT 13 PHP</h1>
+
+    <pre>
+    Y   Y
+     Y Y 
+      Y  
+      Y  
+      Y  
+    </pre>
+
+    <p>
+        SHA256 of GROUP ASSIGNMENT 13 PHP: <?php echo $hash; ?>
+    </p>
+</body>
+</html>
